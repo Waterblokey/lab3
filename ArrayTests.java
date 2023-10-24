a436dd8c97ccdd36c69e23b221ab5b67f138cf0c
Waterblokey <118576768+Waterblokey@users.noreply.github.com>
2023-10-24 09:12:09 -0700

    Tests
    Wrote tests for edge cases in ReverseInPlace() and Reversed()

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+  @Test
+  public void ryantestReverseInPlace() {
+    int [] input1 = { 1, 2 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 2, 1 }, input1);
+  }
+
+  @Test
+  public void ryantestReversed() {
+    int[] input1 = { 1, 2 };
+    assertArrayEquals(new int[]{ 2, 1 }, ArrayExamples.reversed(input1));
+  }
 }
